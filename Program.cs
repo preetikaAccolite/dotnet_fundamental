43ff70cccc866209ca6777d2405e317164283589
Preetika.K_INT1599 <preetika.kandpal@accolitedigital.com>
2024-04-09 15:31:20 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers");
+           int a=Convert.ToInt32(Console.ReadLine());
+            int b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the addition of the numbers is"+Add(a,b));
+            Console.ReadLine();
+        }
+    }
+}
